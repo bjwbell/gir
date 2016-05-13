c657a4546bdfb2c14c49563ef91e469f25d7c9db
JW Bell <bjwbell@gmail.com>
2016-05-13 11:50:06 -0700

    In TestGir(), load file test.gir

--- gir_test.go
+++ gir_test.go
@@ -1,8 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"os"
+	"testing" 
 
-import "testing" 
+	"github.com/bjwbell/gir/config"
+	"github.com/bjwbell/gir/exec"
+	"github.com/bjwbell/gir/parse"
+	"github.com/bjwbell/gir/run"
+	"github.com/bjwbell/gir/scan"
+	"github.com/bjwbell/gir/value"
+)
 
 func TestGir(t *testing.T) {
+	var (
+		conf    config.Config
+		context value.Context
+		fd      io.Reader
+		err     error
+	)
+	context = exec.NewContext(&conf)
+	file := "test.gir"
+	fd, err = os.Open(file)
+	if err != nil {
+		t.Fatalf("gir: %s\n", err)
+	}
+	scanner := scan.New(context, file, bufio.NewReader(fd))
+	parser := parse.NewParser(file, scanner, context)
+	interactive := false
+	ok := run.Run(parser, context, interactive)
+	if !ok {
+		t.Errorf("Error in run.Run")
+	} else {
+		t.Logf("parser: %#v\n", parser)
+	}
 	return
 }
